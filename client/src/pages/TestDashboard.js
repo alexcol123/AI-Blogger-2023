c31c585a6737fee99bd44bf3746ba2858e7b8e40
henry  munoz <henrymunoz@henrys-Mac-mini.local>
2023-03-17 19:29:26 -0400

    front end  test dashboard conditional render links to login , and logout

--- client/src/pages/TestDashboard.js
+++ client/src/pages/TestDashboard.js
@@ -4,12 +4,18 @@ import { BiLogOutCircle } from 'react-icons/bi'
 import { getAuth, signOut } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/auth'
+import { Link } from 'react-router-dom'
 
 const TestDashboard = () => {
   // Ctx
   const [auth, setAuth] = useAuth()
 
+  const { user } = auth
+
   const fbAuth = getAuth()
+
+  // first part of email
+  let userNameFromEmail = user?.email.split('@')[0] || ''
 
   //
   const navigate = useNavigate()
@@ -35,14 +41,32 @@ const TestDashboard = () => {
   }
 
   return (
-    <div>
-      <div className='flex  bg-gray-200 justify-between px-4'>
-        <h4>AI Generator</h4>
-        <ul className='flex  list-none space-x-6 '>
-          <li>Ai Blog</li>
-          <li onClick={logout}>
-            <BiLogOutCircle size={30} className='text-red-500' />
-          </li>
+    <div className=''>
+      <div className='flex  bg-gray-200 justify-between  '>
+        <h2 className='px-3'>AI Generator</h2>
+        <ul className='flex  list-none  justify-center items-center space-x-5 mr-3  '>
+          {user ? (
+            <>
+              {' '}
+              <li>{userNameFromEmail}</li>
+              <li onClick={logout}>
+                <BiLogOutCircle
+                  size={30}
+                  className='text-red-500 bg-white rounded-full '
+                />
+              </li>{' '}
+            </>
+          ) : (
+            <>
+              {' '}
+              <li>
+                <Link to='/loginfb'> Login</Link>
+              </li>
+              <li>
+                <Link to='/loginfb'> Register</Link>
+              </li>{' '}
+            </>
+          )}
         </ul>
       </div>
       <h1>Dashboard</h1>
